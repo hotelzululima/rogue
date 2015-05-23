76f46046d55918579e63fafd0428ba8aeaaa4a7e
enki <enki.matt@outlook.com>
2015-05-23 10:40:25 -0700

    removed common.js

--- src/common.js
+++
@@ -1,9 +0,0 @@
-﻿function forEach(array, callback, scope) {
-    for (var i = 0; i < array.length; i++) {
-        callback.call(scope, i, array[i]);
-    }
-};
-
-function defaultFor(arg, val) {
-    return typeof arg !== 'undefined' ? arg : val;
-}
